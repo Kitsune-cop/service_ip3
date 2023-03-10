2d5edcf01422e2e97b5a0125e816aa5600d3b67b
markTeerasak <twichai4869@gmail.com>
2023-03-10 08:18:46 +0700

    edit path connect

--- admin/student/add_student.php
+++ admin/student/add_student.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT");
 header("Access-Control-Allow-Headers: Content-Type"); 
 
-require_once "../connect.php";
+require_once ".../connect.php";
 $request = json_decode(file_get_contents("php://input"), true);
 try {
 $sql = $conn->prepare("INSERT INTO `student`(`student_id`, `password`, `first_name`, `last_name`, `gender`, `social_id`, `bathday`, `nationality`, `phone_number`, `first_name_father`, `last_name_father`, `first_name_mother`, `last_name_mother`, `first_name_parent`, `last_name_parent`, `phone_number_of_parent`, `status_id`) VALUES 
--- admin/student/update_student.php
+++ admin/student/update_student.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT");
 header("Access-Control-Allow-Headers: Content-Type"); 
 
-require_once "../connect.php";
+require_once ".../connect.php";
 $request = json_decode(file_get_contents("php://input"), true);
 try {
 

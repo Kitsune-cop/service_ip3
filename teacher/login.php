4ac37386c299f0df970d76e6692784d928c51310
markTeerasak <twichai4869@gmail.com>
2023-03-10 14:56:58 +0700

    login

---
+++ teacher/login.php
@@ -0,0 +1,29 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT");
+header("Access-Control-Allow-Headers: Content-Type"); 
+
+require_once "../connect.php";
+require_once "../response.php";
+
+$response = new Response();
+
+$params = array(
+    'username' => $_GET['username'],
+    'password' => $_GET['password']
+);
+
+$sql = "SELECT teacher_id FROM teacher WHERE teacher_id = :username AND password = :password";
+$statement = $conn->prepare($sql);
+$statement->execute($params);
+
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+if(count($result)){
+    $response->success($result, "success", 200);    
+}else {
+    $response->error('error not found!', 404);
+}
+
+?>

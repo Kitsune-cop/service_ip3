b21032a96ac6a4a9560a6cabffc292c1318458d3
KuNFrame <rpf.tv.1234@gmail.com>
2023-04-07 16:59:54 +0700

    add_work_complete

--- teacher/add_work.php
+++ teacher/add_work.php
@@ -5,17 +5,29 @@ header("Access-Control-Allow-Methods: GET, POST, PUT");
 header("Access-Control-Allow-Headers: Content-Type"); 
 
 require_once "../connect.php";
-$request = json_decode(file_get_contents("php://input"), true);
+$request = json_decode(file_get_contents("php://input"), true, JSON_UNESCAPED_UNICODE);
+$params_for_select = array(
+    'subject_id' => $request['subject_id'],
+    'grade' => $request['grade'],
+    'room' => $request['room']
+);
+$params_for_insert = array(
+    'work_name' => $request['work_name'],
+    'work_details' => $request['work_details'],
+    'date' => $request['date']
+);
+
 try {
-$sel = "SELECT enroll.enroll_subject_id FROM enroll JOIN enroll_subject ON enroll.enroll_subject_id = enroll_subject.enroll_subject_id WHERE enroll_subject.subject_id = 'ส22234' ";
-$statement = $conn->prepare($sel);
-$statement->execute($request);
+$sql = "SELECT * FROM enroll JOIN enroll_subject ON enroll.enroll_subject_id = enroll_subject.enroll_subject_id WHERE enroll_subject.subject_id = :subject_id AND enroll.grade = :grade AND enroll.room = :room";
+$statement = $conn->prepare($sql);
+$statement->execute($params_for_select);
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-// print_r($result[0]['enroll_subject_id']);
-$sql = $conn->prepare("INSERT INTO `work`(`work_id`, `work_name`, `work_details`, `deadline`, `enroll_subject_id`) VALUES 
-(:work_name,:work_details,$result[0]['enroll_subject_id'])");
-print_r($result[0]['enroll_subject_id']);
-if($sql->execute($request)){
+// if(count($result)){
+//     echo json_encode($result, JSON_UNESCAPED_UNICODE);
+// }
+$sql = $conn->prepare("INSERT INTO work(work_id, work_name, work_details, deadline, enroll_subject_id) VALUES 
+('', :work_name, :work_details,:date,". $result[0]['enroll_subject_id']. ")");
+if($sql->execute($params_for_insert)){
     $response = array(
         'status' => true,
         'message' => 'success'
@@ -26,9 +38,6 @@ if($sql->execute($request)){
     $response = array(
         'status' => false,
         'message' => 'error',
-        'sql' => $sql,
-        'data' => $data,
-        'request' => gettype($request), 
         'error' => $e
     );
     echo json_encode($response, JSON_UNESCAPED_UNICODE);
--- test.php
+++ test.php
@@ -1,10 +1,44 @@
 <?php
-// require './connect.php';
-// require_once './classroom/classroom.php';
-// $params = array(
-//     'school_year' => '2019',
-//     'grade' => '1',
-//     'room' => '4'
-// );
-// check_classroom_and_add($params);
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT");
+header("Access-Control-Allow-Headers: Content-Type"); 
+
+require_once "./connect.php";0
+$request = json_decode(file_get_contents("php://input"), true, JSON_UNESCAPED_UNICODE);
+$params = array('subject_id'=>$request['subject_id']);
+$work_name = $request['work_name'];
+$work_details = $request['work_details'];
+// $sub = $request['subject_id'];
+// print_r($request);
+// print_r($request['work_name']);
+try {
+$sql = "SELECT enroll.enroll_subject_id FROM enroll JOIN enroll_subject ON enroll.enroll_subject_id = enroll_subject.enroll_subject_id WHERE enroll_subject.subject_id = :subject_id";
+$statement = $conn->prepare($sql);
+$statement->execute($params);
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+// print(count($result));
+// print_r($params);
+// print_r($result);
+$res = $result[0]['enroll_subject_id'];
+// print_r($res);
+$sql = $conn->prepare("INSERT INTO `work`(`work_id`, `work_name`, `work_details`, `deadline`, `enroll_subject_id`) VALUES 
+('',$work_name,$work_details,'',$res)");
+// print_r($result[0]['enroll_subject_id']);
+if($sql->execute($request)){
+    $response = array(
+        'status' => true,
+        'message' => 'success'
+    );
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+}
+} catch(PDOException $e) {
+    $response = array(
+        'status' => false,
+        'message' => 'error',
+        'request' => $request, 
+        'error' => $e
+    );
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+}
 ?>

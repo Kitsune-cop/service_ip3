efe5e4c37e8453d3357b74d1b72fb3a8c20fd8fe
KuNFrame <rpf.tv.1234@gmail.com>
2023-04-08 16:08:47 +0700

    stu_schedule

--- student/schedule.php
+++ student/schedule.php
@@ -1,31 +1,49 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type"); 
 
-require_once('../connect.php');
-require_once('../response.php');
+require_once "../connect.php";
+require_once "../response.php";
 
 $response = new Response();
 
 $params = array(
-    'grade' => $_GET['grade'],
-    'room' => $GET_['room']
+    'id' => $_GET['id'],
 );
 
-$sql = "SELECT * FROM student_has_class 
-INNER JOIN time_table ON student_has_class.school_year = time_table.school_year 
-INNER JOIN enroll_subject ON time_table.enroll_subject_id = enroll_subject.enroll_subject_id 
-WHERE time_table.grade = :grade AND time_table.room = :room AND student_has_class.school_year = '2019' ";
+try{
+$sql = "SELECT *
+FROM `time_table` 
+INNER JOIN enroll_subject 
+ON time_table.enroll_subject_id = enroll_subject.enroll_subject_id 
+INNER JOIN subject
+ON enroll_subject.subject_id = subject.subject_id
+INNER JOIN enroll
+ON enroll.enroll_subject_id = enroll_subject.enroll_subject_id
+WHERE enroll.student_id = :id";
+
+
 $statement = $conn->prepare($sql);
 $statement->execute($params);
-
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-if (count($result)) {
-    $response->success($result);
-} else {
-    $response->error();
+
+if(count($result)){
+    $response->success($result, "success", 200);    
+}else {
+    // $response->error('error not found!', 404);
+    echo json_encode($params, JSON_UNESCAPED_UNICODE);
+}
+} catch(PDOException $e) {
+    $response = array(
+        'status' => false,
+        'message' => 'error',
+        'params' => $params,
+        'error' => $e
+    );
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
 }
 
 ?>
---
+++ student/showSubject.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT");
+header("Access-Control-Allow-Headers: Content-Type");
+
+require_once('../connect.php');
+require_once('../response.php');
+
+$response = new Response();
+
+$params = array(
+    'id' => $_GET['id'],
+);
+
+$sql = "SELECT * FROM subject
+WHERE subject.subject_id = :id ";
+$statement = $conn->prepare($sql);
+$statement->execute($params);
+
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($result)) {
+    $response->success($result);
+} else {
+    $response->error();
+}
+
+?>

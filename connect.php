edf32f4d9e1fdc852a26bac7e56b7368a5be3ba6
Mr.Kitsune <84368356+Kitsune-cop@users.noreply.github.com>
2023-04-08 20:53:29 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=school3", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=school2", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   

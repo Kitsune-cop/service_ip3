baec88d808fed0e87fc952955d243815895a9029
KuNFrame <rpf.tv.1234@gmail.com>
2023-04-08 15:47:24 +0700

    add_schedule

---
+++ teacher/schedule.php
@@ -0,0 +1,49 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT");
+header("Access-Control-Allow-Headers: Content-Type"); 
+
+require_once "../connect.php";
+require_once "../response.php";
+
+$response = new Response();
+
+$params = array(
+    'id' => $_GET['id'],
+);
+
+try{
+$sql = "SELECT *
+FROM `time_table` 
+INNER JOIN enroll_subject 
+ON time_table.enroll_subject_id = enroll_subject.enroll_subject_id 
+INNER JOIN subject
+ON enroll_subject.subject_id = subject.subject_id
+INNER JOIN teacher
+ON enroll_subject.teacher_id = teacher.teacher_id 
+WHERE teacher.teacher_id = :id";
+
+
+$statement = $conn->prepare($sql);
+$statement->execute($params);
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+if(count($result)){
+    $response->success($result, "success", 200);    
+}else {
+    // $response->error('error not found!', 404);
+    echo json_encode($params, JSON_UNESCAPED_UNICODE);
+}
+} catch(PDOException $e) {
+    $response = array(
+        'status' => false,
+        'message' => 'error',
+        'params' => $params,
+        'error' => $e
+    );
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+}
+
+?>
